8d103dea6e7c6d24a9ad19221e95a77a565ffb98
lenok0113 <lenok0113@mail.ru>
2019-10-26 15:57:24 +0300

    lesso1_2

--- src/main/java/com/ifmo/lesson2/DigitsSum.java
+++ src/main/java/com/ifmo/lesson2/DigitsSum.java
@@ -9,7 +9,7 @@ public class DigitsSum {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Input a number: ");
         int n = scanner.nextInt();
 
         int digitSum = digitSum(n);
@@ -20,6 +20,16 @@ public class DigitsSum {
     public static int digitSum(int n) {
         // TODO implement
 
-        return 0;
+
+        int sum = 0;
+
+        while (n > 0) {
+            int num = n % 10;
+            sum += num;
+            n -= num;
+            n /= 10;
+        }
+
+        return sum;
     }
 }
--- src/main/java/com/ifmo/lesson2/Dividers.java
+++ src/main/java/com/ifmo/lesson2/Dividers.java
@@ -9,9 +9,26 @@ public class Dividers {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+                int n = scanner.nextInt();
 
-        int n = scanner.nextInt();
+        int digitSum = digitSum(n);
 
+        System.out.println(digitSum);
+    }
+
+    public static int digitSum(int n) {
         // TODO implement
+
+        int sum = 0;
+
+        while (n > 0) {
+            int num = n % 10;
+            sum += num;
+            n -= num;
+            n /= 10;
+        }
+
+        return sum;
     }
 }
