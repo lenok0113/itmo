e917c4f79667ef18ba8658993ff21a5ba3e5b9da
lenok0113 <lenok0113@mail.ru>
2019-10-26 20:41:11 +0300

    lesso1_6

---
+++ src/main/java/com/ifmo/lesson6/LinkedList.java
@@ -0,0 +1,65 @@
+package com.ifmo.lesson6;
+
+import java.util.Iterator;
+
+/**
+ * Односвязный список, где каждый предыдущий
+ * элемент харнит ссылку на следующий. Список
+ * оканчивается ссылкой со значением {@code null}.
+ */
+public class LinkedList implements List, Stack, Queue {
+    /** Ссылка на первый элемент списка. */
+    private Item head;
+
+    /** {@inheritDoc} */
+    @Override
+    public void add(Object val) {
+        // TODO implement.
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object take() {
+        // TODO implement.
+
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object get(int i) {
+        // TODO implement.
+
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object remove(int i) {
+        // TODO implement.
+
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Iterator iterator() {
+        // TODO implement.
+
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void push(Object value) {
+        // TODO implement.
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object pop() {
+        // TODO implement.
+
+        return null;
+    }
+}
